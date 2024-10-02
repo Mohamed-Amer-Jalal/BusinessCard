78704e8ed819e3f5c20328e59ea9fbadb1463c1a
Mohamed Amer <mohamedamerjalal@gmail.com>
2024-10-02 05:07:13 +0300

    BusinessCard 4th

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -10,10 +10,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
@@ -29,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,7 +50,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BusinessCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val layoutDirection = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateStartPadding(LocalLayoutDirection.current)
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding()
+                        .padding(start = layoutDirection, end = layoutDirection)
+                ) { innerPadding ->
                     BusinessCard(
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -127,6 +141,18 @@ fun ContactRow(text: String, icon: ImageVector) {
 @Composable
 fun BusinessCardPreview() {
     BusinessCardTheme {
-        BusinessCard()
+        val layoutDirection = WindowInsets.safeDrawing
+            .asPaddingValues()
+            .calculateStartPadding(LocalLayoutDirection.current)
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .padding(start = layoutDirection, end = layoutDirection)
+        ) { innerPadding ->
+            BusinessCard(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
